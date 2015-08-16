85fc362d6c903e856b500789021430abc61fbd93
Julia Van Cleve <julia@jmv.me>
2015-08-16 03:11:00 -0700

    Convert gallery from Simpleviewer to Lightbox

--- js/display_images.js
+++ js/display_images.js
@@ -1,24 +1,16 @@
 $(document).ready(function() {
 
-  var json_link = "http://localhost:3000/photos"
-  var photo_site = "http://localhost:3000"
+  var json_link = "https://julia-photo-uploader.herokuapp.com/users/2"
 
   var result = $.getJSON( json_link, {
     format: "json"
   })
     .done(function(result) {
       for (i in result) {
-        if (result[i].album == "1") {
-          photo_title = result[i].title
-          
-          photo_location = result[i].image.image.resized.url
-          photo_url = photo_site + photo_location
-          
-          thumb_location = result[i].image.image.small_thumb.url
-          thumb_url = photo_site + thumb_location
-          
-          $("div#content").append(" <a href=' " + photo_url + " ' data-lightbox='gallery' data-title=' " + photo_title + " '><img src=' " + thumb_url + "' />")
-        }
+        photo_title = result[i].title
+        photo_location = result[i].image.image.resized.url
+        thumb_location = result[i].image.image.thumb.url
+        $("div#content").append(" <a href=' " + photo_location + " ' data-lightbox='gallery' data-title=' " + photo_title + " '><img src=' " + thumb_location + "' />")
       }
     })
 });
